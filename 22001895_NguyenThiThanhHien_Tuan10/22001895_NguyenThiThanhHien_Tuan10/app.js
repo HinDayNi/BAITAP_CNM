f2fa9565f76dea6c940133f84164d5112c57f544
HinDayNi <gthithanhhien04@gmail.com>
2026-03-09 16:16:33 +0700

    done

--- 22001895_NguyenThiThanhHien_Tuan10/22001895_NguyenThiThanhHien_Tuan10/app.js
+++ 22001895_NguyenThiThanhHien_Tuan10/22001895_NguyenThiThanhHien_Tuan10/app.js
@@ -18,7 +18,9 @@ const productRoutes = require("./routes/productRoutes")
 
 app.use("/", productRoutes)
 
-app.listen(3000, () => {
-    console.log("Server is running on port 3000")
-})
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
 
